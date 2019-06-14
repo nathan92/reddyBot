a1b0b3dc027a22bd6372e134a9cf9b8fc04d9a92
nathan92 <nathan92oldfield@gmail.com>
2019-06-14 13:26:39 +0100

    added basic dynamic user input and modified main loop accordingly, refactored hero categories for guide objects to make comparisons easier, general code tidying

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 ## Current functionality:
 - Trawls the latest posts on Overwatch University subreddit to return guides related to a defined list of heroes; outputs those guides to terminal
+- Uses the PRAW API
 
 ## To Do:
-- Dynamic, user defined heroes
+- Improve flexibility of user defined heroes/categories
 - Use a library for more robust/fuzzy string matching to account for spelling mistakes in titles
 - Add articles to database for later access
 - Change from terminal output to some other form of web notifications
 - Set up so can be run as a script on a server/RPi
+- Language processing to differentiate questions from guides
--- owuBot.py
+++ owuBot.py
@@ -1,15 +1,61 @@
-import praw
+##--------------------------------------------------------------------------------------------------------##
+##----------------------------------------Imports, definitions--------------------------------------------##
+##--------------------------------------------------------------------------------------------------------##
 
-##--------------------------------------------------------------------------------------------------------##
-##--------------------------------------------------------------------------------------------------------##
+import praw
+import sys
 
 # Strings for matching heroes of interest in post titles
-reinStrings = ['Reinhardt', 'reinhardt', 'rein', 'Rein']
-orisaStrings = ['Orisa', 'orisa']
-winstonStrings = ['Monkey', 'monkey', 'winston', 'Winston']
+reinStrings = ['Reinhardt', 'Rein']
+orisaStrings = ['Orisa']
+winstonStrings = ['Winston', 'Monkey']
+mercyStrings = ['Mercy']
+anaStrings = ['Ana']
+zenStrings = ['Zen']
+soldierStrings = ['Soldier']
+symmetraStrings = ['Symmetra']
 
-# Uses ANSI formatting; prints multiple hero categroies to the same line
+##--------------------------------------------------------------------------------------------------------##
+##-------------------------------------------Classes/Functions--------------------------------------------##
+##--------------------------------------------------------------------------------------------------------##
+
+# The guide object representing a given guide or post on the OWU subreddit
+class Guide:
+
+	def __init__(self, title, includedHeroes, url):
+		self.title = title
+		self.includedHeroes = tuple(includedHeroes)
+		self.url = url
+
+	def printGuide(self):
+		print('\n')
+		print('\x1b[4;35;40m' + '---' + self.title + '---' + '\x1b[0m')
+		print('URL: ' + self.url)
+
+	def isInCategory(self, categoryTuple):
+		return categoryTuple == self.includedHeroes
+
+def getUserHeroes():
+
+	matchingStringLists = []
+	userInput = int(input("Preset number: "))
+
+	if userInput > 3 or userInput < 1:
+		print("Invalid selection, defaulting to 1")
+		matchingStringLists.extend([reinStrings, orisaStrings, winstonStrings])
+		return matchingStringLists
+
+	if userInput == 1:
+		matchingStringLists.extend([reinStrings, orisaStrings, winstonStrings])
+	elif userInput == 2:
+		matchingStringLists.extend([mercyStrings, anaStrings, zenStrings])
+	elif userInput == 3: 
+		matchingStringLists.extend([soldierStrings, symmetraStrings])
+	return matchingStringLists
+
+# Uses ANSI formatting; prints compound hero categroies to the same line
 def printCategoryTitle(heroes):
+
 	print('\n' + '\x1b[1;35;40m')
 	if len(heroes) > 1:
 		for index, hero in enumerate(heroes):
@@ -21,78 +67,58 @@ def printCategoryTitle(heroes):
 		print('\x1b[1;35;40m' + heroes[0], end=' ')
 	print('guides: ' + '\x1b[0m' + '\n')
 
-# The guide object representing a given guide or post on the OWU subreddit
-class Guide:
-
-	def __init__(self, title, includedHeroes, url):
-		self.title = title
-		self.includedHeroes = includedHeroes
-		self.url = url
-
-	def printGuide(self):
-		print('\n')
-		print('\x1b[4;35;40m' + '---' + self.title + '---' + '\x1b[0m')
-		print('URL: ' + self.url)
-
-	# Category types for the day are stored as a set of tuples, here we check for equality of the guide 
-	# object's list of included heroes against those tuples:
-	def isInCategory(self, categoryTuple):
-		if len(categoryTuple) != len (self.includedHeroes):
-			return False
-
-		for category in categoryTuple:
-			categoryIsIncluded = False
-			for hero in self.includedHeroes:
-				if hero == category:
-					categoryIsIncluded = True
-					break
-			if categoryIsIncluded == False:
-				return False
-
-		return True
+def printGenericTitle(titleString):
+	print('\x1b[1;35;40m' + titleString + '\n' + '\x1b[0m')
 
 ##--------------------------------------------------------------------------------------------------------##
+##------------------------------------------Functionality-------------------------------------------------##
 ##--------------------------------------------------------------------------------------------------------##
 
-#Use praw API to interface with reddit and fetch the new posts from reddit.com/overwatchuniversity
+printGenericTitle("OWU scraper")
+print("-----------")
+print("Please select hero preset:")
+print("1) Main tanks: Rein, Orisa, Winston")
+print("2) Healers: Mercy, Zen, Ana")
+print("3) DPS: Soldier, Symmetra")
+print("-----------")
+
+# Use praw API to interface with reddit and fetch the new posts from reddit.com/overwatchuniversity
 reddit = praw.Reddit('bot1', user_agent='<Python:reddyBot:v1.0 (by /u/reddyBootboot)')
 newPosts = reddit.subreddit('overwatchuniversity').new()
+
+heroStringLists = getUserHeroes()
 
 todaysGuides = []
 todaysHeroCombinations = set()
 
-#Iterate through today's new posts, and for each post which mentions one or more of the heroes of interest
-#create a guide object for that post and add the hero combination as a category for today's guides:
+# Iterate through today's new posts, and for each post which mentions one or more of the heroes of interest
+# create a guide object for that post and add the hero combination as a category for today's guides:
 for submission in newPosts:
 
 	heroesInCurrentSubmission = []
 
-	for reinString in reinStrings:
-		if reinString in submission.title:
-			heroesInCurrentSubmission.append('Reinhardt')
-			break
-	for orisaString in orisaStrings:
-		if orisaString in submission.title:
-			heroesInCurrentSubmission.append('Orisa')
-			break
-	for winstonString in winstonStrings:
-		if winstonString in submission.title:
-			heroesInCurrentSubmission.append('Winston')
-			break
+	for heroStringList in heroStringLists:
+		for heroString in heroStringList:
+			if heroString.lower() in submission.title.lower():
+				heroesInCurrentSubmission.append(heroStringList[0])
+				break
 
 	if len(heroesInCurrentSubmission) > 0:
 		todaysHeroCombinations.add(tuple(heroesInCurrentSubmission))
 		todaysGuides.append(Guide(submission.title, heroesInCurrentSubmission, submission.url))
 
-#Output:
-print('\x1b[1;35;40m' + 'Today\'s Main-tank guides featured on OWU:\n' + '\x1b[0m')
-
+# Output:
 if len(todaysHeroCombinations) > 0:
+	printGenericTitle('Today\'s relevant guides featured on OWU:')
+
 	for heroCombination in todaysHeroCombinations:
 		printCategoryTitle(heroCombination)
 		for guide in todaysGuides:
 			if guide.isInCategory(heroCombination):
 				guide.printGuide()
+else:
+	printGenericTitle('Sorry, nothing today')
+
 
 ##--------------------------------------------------------------------------------------------------------##
 ##--------------------------------------------------------------------------------------------------------##
