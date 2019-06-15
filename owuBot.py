79317aed2f8f9a1a4d422a7cdf948b852344b4db
nathan92 <nathan92oldfield@gmail.com>
2019-06-15 20:27:31 +0100

    moved strings to a separate file

--- owuBot.py
+++ owuBot.py
@@ -3,16 +3,7 @@
 ##--------------------------------------------------------------------------------------------------------##
 
 import praw
-
-# Strings for matching heroes of interest in post titles
-reinStrings = ['Reinhardt', 'Rein']
-orisaStrings = ['Orisa']
-winstonStrings = ['Winston', 'Monkey']
-mercyStrings = ['Mercy']
-anaStrings = ['Ana']
-zenStrings = ['Zen']
-soldierStrings = ['Soldier']
-symmetraStrings = ['Symmetra']
+from owuStrings import *
 
 ##--------------------------------------------------------------------------------------------------------##
 ##-------------------------------------------Classes/Functions--------------------------------------------##
---
+++ owuStrings.py
@@ -0,0 +1,9 @@
+# Strings for matching heroes of interest in post titles
+reinStrings = ['Reinhardt', 'Rein']
+orisaStrings = ['Orisa']
+winstonStrings = ['Winston', 'Monkey']
+mercyStrings = ['Mercy']
+anaStrings = ['Ana']
+zenStrings = ['Zen']
+soldierStrings = ['Soldier']
+symmetraStrings = ['Symmetra']
